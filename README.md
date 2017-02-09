7a56fb2b5ec9e34b9158b305fa732e27b0b0df34
Mike Lynch <mike.lynch@iinet.net.au>
2017-02-10 10:50:17 +1100

    First quick version

---
+++ README.md
@@ -0,0 +1,5 @@
+The Pedestrian d3
+=================
+
+Implementation of Lisa Roberts' Pedestrian character using the d3 force layout.
+
---
+++ pedestrian.js
@@ -0,0 +1,52 @@
+pedestrian = { "nodes": [
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+    { "x": 480, "y": 250 },
+  ],
+
+  "links": [
+        { "source": 0, "target": 1, "length": 50 },
+        { "source": 0, "target": 16, "length": 35 },
+        { "source": 0, "target": 17, "length": 35 },
+        { "source": 16, "target": 17, "length": 50 },
+        { "source": 16, "target": 1, "length": 35 },
+        { "source": 17, "target": 1, "length": 35 },
+
+        { "source": 2, "target": 1, "length": 50 },
+        { "source": 3, "target": 1, "length": 50 },
+
+        { "source": 4, "target": 2, "length": 50 },
+        { "source": 5, "target": 4, "length": 50 },
+
+        { "source": 3, "target": 6, "length": 50 },
+        { "source": 6, "target": 7, "length": 50 },
+
+        { "source": 1, "target": 8, "length": 40 },
+        { "source": 8, "target": 10, "length": 100 },
+        { "source": 8, "target": 11, "length": 100 },
+
+        { "source": 2, "target": 9, "length": 100 },
+        { "source": 3, "target": 9, "length": 100 },
+        { "source": 9, "target": 10, "length": 50 },
+        { "source": 9, "target": 11, "length": 50 },
+        { "source": 11, "target": 13, "length": 100 },
+        { "source": 13, "target": 15, "length": 100 },
+        { "source": 10, "target": 12, "length": 100 },
+        { "source": 12, "target": 14, "length": 100 }
+    ]
+             };
