a209c73b7163480413d2a8e78caf995b8ff04150
Mike Lynch <mike.lynch@iinet.net.au>
2017-02-10 13:36:03 +1100

    Made the model smaller, removed gravity, adjusted charge

--- pedestrian.js
+++ pedestrian.js
@@ -20,33 +20,33 @@ pedestrian = { "nodes": [
   ],
 
   "links": [
-        { "source": 0, "target": 1, "length": 50 },
-        { "source": 0, "target": 16, "length": 35 },
-        { "source": 0, "target": 17, "length": 35 },
-        { "source": 16, "target": 17, "length": 50 },
-        { "source": 16, "target": 1, "length": 35 },
-        { "source": 17, "target": 1, "length": 35 },
+        { "source": 0, "target": 1, "length": 30 },
+        { "source": 0, "target": 16, "length": 20 },
+        { "source": 0, "target": 17, "length": 20 },
+        { "source": 16, "target": 17, "length": 30 },
+        { "source": 16, "target": 1, "length": 20 },
+        { "source": 17, "target": 1, "length": 20 },
 
-        { "source": 2, "target": 1, "length": 50 },
-        { "source": 3, "target": 1, "length": 50 },
+        { "source": 2, "target": 1, "length": 30 },
+        { "source": 3, "target": 1, "length": 30 },
 
-        { "source": 4, "target": 2, "length": 50 },
-        { "source": 5, "target": 4, "length": 50 },
+        { "source": 4, "target": 2, "length": 30 },
+        { "source": 5, "target": 4, "length": 30 },
 
-        { "source": 3, "target": 6, "length": 50 },
-        { "source": 6, "target": 7, "length": 50 },
+        { "source": 3, "target": 6, "length": 30 },
+        { "source": 6, "target": 7, "length": 30 },
 
-        { "source": 1, "target": 8, "length": 40 },
-        { "source": 8, "target": 10, "length": 100 },
-        { "source": 8, "target": 11, "length": 100 },
+        { "source": 1, "target": 8, "length": 20 },
+        { "source": 8, "target": 10, "length": 60 },
+        { "source": 8, "target": 11, "length": 60 },
 
-        { "source": 2, "target": 9, "length": 100 },
-        { "source": 3, "target": 9, "length": 100 },
-        { "source": 9, "target": 10, "length": 50 },
-        { "source": 9, "target": 11, "length": 50 },
-        { "source": 11, "target": 13, "length": 100 },
-        { "source": 13, "target": 15, "length": 100 },
-        { "source": 10, "target": 12, "length": 100 },
-        { "source": 12, "target": 14, "length": 100 }
+        { "source": 2, "target": 9, "length": 60 },
+        { "source": 3, "target": 9, "length": 60 },
+        { "source": 9, "target": 10, "length": 30 },
+        { "source": 9, "target": 11, "length": 30 },
+        { "source": 11, "target": 13, "length": 60 },
+        { "source": 13, "target": 15, "length": 60 },
+        { "source": 10, "target": 12, "length": 60 },
+        { "source": 12, "target": 14, "length": 60 }
     ]
              };
