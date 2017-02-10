6c3727dbc6f9b5cab3bd121f961f29f69e763cab
Mike Lynch <mike.lynch@iinet.net.au>
2017-02-10 16:55:07 +1100

    More refinements of the v4 port

--- pedestrian.js
+++ pedestrian.js
@@ -20,34 +20,34 @@ pedestrian = { "nodes": [
   ],
 
   "links": [
-        { "source": "head", "target": "neck", "value": 30 },
-        { "source": "head", "target": "rear", "value": 20 },
-        { "source": "head", "target": "lear", "value": 20 },
-        { "source": "rear", "target": "lear", "value": 30 },
-        { "source": "neck", "target": "rear", "value": 20 },
-        { "source": "neck", "target": "lear", "value": 20 },
+        { "source": "head", "target": "neck", "length": 30 },
+        { "source": "head", "target": "rear", "length": 20 },
+        { "source": "head", "target": "lear", "length": 20 },
+        { "source": "rear", "target": "lear", "length": 30 },
+        { "source": "neck", "target": "rear", "length": 20 },
+        { "source": "neck", "target": "lear", "length": 20 },
 
-        { "source": "neck", "target": "rshoulder", "value": 30 },
-        { "source": "rshoulder", "target": "relbow", "value": 50 },
-        { "source": "relbow", "target": "rhand", "value": 50 },
+        { "source": "neck", "target": "rshoulder", "length": 30 },
+        { "source": "rshoulder", "target": "relbow", "length": 50 },
+        { "source": "relbow", "target": "rhand", "length": 50 },
 
-        { "source": "neck", "target": "lshoulder", "value": 30 },
-        { "source": "lshoulder", "target": "lelbow", "value": 50 },
-        { "source": "lelbow", "target": "lhand", "value": 50 },
+        { "source": "neck", "target": "lshoulder", "length": 30 },
+        { "source": "lshoulder", "target": "lelbow", "length": 50 },
+        { "source": "lelbow", "target": "lhand", "length": 50 },
 
-        { "source": "neck", "target": "heart", "value": 30 },
-        { "source": "heart", "target": "rhip", "value": 50 },
-        { "source": "heart", "target": "lhip", "value": 50 },
+        { "source": "neck", "target": "heart", "length": 30 },
+        { "source": "heart", "target": "rhip", "length": 50 },
+        { "source": "heart", "target": "lhip", "length": 50 },
 
-        { "source": "rshoulder", "target": "groin", "value": 60 },
-        { "source": "lshoulder", "target": "groin", "value": 60 },
-        { "source": "groin", "target": "rhip", "value": 30 },
-        { "source": "groin", "target": "lhip", "value": 30 },
+        { "source": "rshoulder", "target": "groin", "length": 60 },
+        { "source": "lshoulder", "target": "groin", "length": 60 },
+        { "source": "groin", "target": "rhip", "length": 30 },
+        { "source": "groin", "target": "lhip", "length": 30 },
 
-        { "source": "rhip", "target": "rknee", "value": 50 },
-        { "source": "rknee", "target": "rfoot", "value": 50 },
+        { "source": "rhip", "target": "rknee", "length": 50 },
+        { "source": "rknee", "target": "rfoot", "length": 50 },
 
-        { "source": "lhip", "target": "lknee", "value": 50 },
-        { "source": "lknee", "target": "lfoot", "value": 50 },
+        { "source": "lhip", "target": "lknee", "length": 50 },
+        { "source": "lknee", "target": "lfoot", "length": 50 },
     ]
              };
