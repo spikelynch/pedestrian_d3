7f6b10d35a05051955d3d348c311b70535bbc368
Mike Lynch <mike.lynch@iinet.net.au>
2017-04-13 15:47:27 +1000

    Reformatting before splitting

--- pedestrian.js
+++ pedestrian.js
@@ -1,4 +1,12 @@
-var PEDESTRIAN = {
+// Pedestrian_d3 configuration
+
+
+// MODEL defines the nodes and the links which connect them
+
+var MODEL = {
+
+    // List of nodes. Each node's ID should be unique.
+    
     "nodes": [
         { "id": "head" },
         { "id": "rear" },
@@ -20,38 +28,50 @@ var PEDESTRIAN = {
         { "id": "lfoot" }
     ],
 
+    // List of links.
+    //
+    // Each link has:
+    //   source, target - the IDs of two nodes which it connects.
+    //                    (the direction doesn't matter)
+    //   length         - the default length of the node
+    
     "links": [
-        { "source": "head", "target": "neck", "length": 30 },
-        { "source": "head", "target": "rear", "length": 20 },
-        { "source": "head", "target": "lear", "length": 20 },
-        { "source": "rear", "target": "lear", "length": 30 },
-        { "source": "neck", "target": "rear", "length": 20 },
-        { "source": "neck", "target": "lear", "length": 20 },
+        { "source": "head",      "target": "neck",      "length": 30 },
+        { "source": "head",      "target": "rear",      "length": 20 },
+        { "source": "head",      "target": "lear",      "length": 20 },
+        { "source": "rear",      "target": "lear",      "length": 30 },
+        { "source": "neck",      "target": "rear",      "length": 20 },
+        { "source": "neck",      "target": "lear",      "length": 20 },
         
-        { "source": "neck", "target": "rshoulder", "length": 30 },
-        { "source": "rshoulder", "target": "relbow", "length": 50 },
-        { "source": "relbow", "target": "rhand", "length": 50 },
-
-        { "source": "neck", "target": "lshoulder", "length": 30 },
-        { "source": "lshoulder", "target": "lelbow", "length": 50 },
-        { "source": "lelbow", "target": "lhand", "length": 50 },
-
-        { "source": "neck", "target": "heart", "length": 30 },
-        { "source": "heart", "target": "rhip", "length": 50 },
-        { "source": "heart", "target": "lhip", "length": 50 },
-
-        { "source": "rshoulder", "target": "groin", "length": 60 },
-        { "source": "lshoulder", "target": "groin", "length": 60 },
-        { "source": "groin", "target": "rhip", "length": 30 },
-        { "source": "groin", "target": "lhip", "length": 30 },
-
-        { "source": "rhip", "target": "rknee", "length": 50 },
-        { "source": "rknee", "target": "rfoot", "length": 50 },
-
-        { "source": "lhip", "target": "lknee", "length": 50 },
-        { "source": "lknee", "target": "lfoot", "length": 50 },
+        { "source": "neck",      "target": "rshoulder", "length": 30 },
+        { "source": "rshoulder", "target": "relbow",    "length": 50 },
+        { "source": "relbow",    "target": "rhand",     "length": 50 },
+        
+        { "source": "neck",      "target": "lshoulder", "length": 30 },
+        { "source": "lshoulder", "target": "lelbow",    "length": 50 },
+        { "source": "lelbow",    "target": "lhand",     "length": 50 },
+        
+        { "source": "neck",      "target": "heart",     "length": 30 },
+        { "source": "heart",     "target": "rhip",      "length": 50 },
+        { "source": "heart",     "target": "lhip",      "length": 50 },
+        
+        { "source": "rshoulder", "target": "groin",     "length": 60 },
+        { "source": "lshoulder", "target": "groin",     "length": 60 },
+        { "source": "groin",     "target": "rhip",      "length": 30 },
+        { "source": "groin",     "target": "lhip",      "length": 30 },
+        
+        { "source": "rhip",      "target": "rknee",     "length": 50 },
+        { "source": "rknee",     "target": "rfoot",     "length": 50 },
+        
+        { "source": "lhip",      "target": "lknee",     "length": 50 },
+        { "source": "lknee",     "target": "lfoot",     "length": 50 }
     ]
 };
+
+
+// Soundfiles to be triggered when the user moves the mouse pointer
+// over a node. Keys should be node IDs.
+// File paths are relative to /media/sounds/
 
 
 var SOUNDFILES = {
@@ -74,6 +94,12 @@ var SOUNDFILES = {
     "lknee": "2017-02-23-17Frog-Birds.mp3",
     "lfoot": "2017-02-23-18Frog-Birds.mp3"
 };
+
+// Video clips to be played when the user drags and releases a node.
+// Values are:
+// -  file - file path relative to /media/videos
+// -  x, y - position of the video window, relative to the top left corner
+// -  w, h - width and height of video in pixels
 
 
 var VIDEOS = {
